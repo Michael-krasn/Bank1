4a53c988294bf20e6b5f0269913089f21c8971fd
Mikhail Krasnoperov <mikhail.krasnoperov@mail.ru>
2025-10-01 11:03:24 +0300

    Added new features to the project

---
+++ src/widget.py
@@ -0,0 +1,29 @@
+def mask_account_card(data: str) -> str:
+    """
+    Функция для маскировки номера карты или счета.
+    """
+    # Разделяем строку на тип и номер
+    parts = data.split()
+
+    # Если это счет
+    if parts[0] == "Счет":
+        account_number = parts[-1]
+        # Маскируем последние 4 цифры
+        masked_acc = f"**{account_number[-4:]}"
+        return f"{parts[0]} {masked_acc}"
+
+    # Если это карта
+    else:
+        card_number = parts[-1]
+        card_name = " ".join(parts[:-1])
+        # Маскируем номер карты
+        masked_card = (
+            f"{card_number[:4]} "
+            f"{card_number[4:6]}** "
+            f"**** "
+            f"{card_number[-4:]}"
+        )
+        return f"{card_name} {masked_card}"
+
+print(mask_account_card("Visa Platinum 7000792289606361"))  # Visa Platinum 7000 79** **** 6361
+print(mask_account_card("Счет 73654108430135874305"))      # Счет **4305
